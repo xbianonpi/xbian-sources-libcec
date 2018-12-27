22857993822ca22c9c578fb2e55bd6ad13effa24
mkreisl <manfred.kreisl@nexgo.de>
2018-12-27 17:44:37 +0100

    [IMX] fix after https://github.com/Pulse-Eight/libcec/commit/2c41938e000026725487cc0f0ba4b82f11a70837

--- src/libcec/adapter/IMX/IMXCECAdapterCommunication.h
+++ src/libcec/adapter/IMX/IMXCECAdapterCommunication.h
@@ -94,6 +94,7 @@ namespace CEC
     bool IsRunningLatestFirmware(void) { return true; }
     bool SaveConfiguration(const libcec_configuration & UNUSED(configuration)) { return false; }
     bool GetConfiguration(libcec_configuration & UNUSED(configuration)) { return false; }
+    bool SetAutoMode(bool UNUSED(automode)) { return false; }
     std::string GetPortName(void) { return std::string("IMX"); }
     uint16_t GetPhysicalAddress(void);
     bool SetControlledMode(bool UNUSED(controlled)) { return true; }
